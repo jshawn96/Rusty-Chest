0ee923c0370303f4db21e7cf3f861a37b1cacec8
Jason Hall <jason@mahletconsulting.com>
2020-03-26 12:13:18 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Rusty Chest
+
+A clean password manager built with Rust
+
+## Installation
+
+Fedora
+
+```bash
+dnf install cargo rustc openssl-devel
+```
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+extern crate rand;
+use rand::Rng;
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    println!("Random f64: {}", rng.gen::<f64>());
+}
